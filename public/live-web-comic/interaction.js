db4ba1d2965bbdb49bb0066a708b188b6b6271c8
lmchun <lmchun@sbcglobal.net>
2023-03-30 20:57:12 -0400

    starting to add in hover action help

---
+++ public/live-web-comic/interaction.js
@@ -0,0 +1,12 @@
+const characterBtn= document.getElementById("chooseCharacter")
+
+function characterHelpShow(){
+
+    // characterBtn.style.visibility = "show";
+
+}
+
+function characterHelpHide(){
+
+    // characterBtn.style.visibility = "show";
+}
